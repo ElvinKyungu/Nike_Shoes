67436d1d8c344444a5ee1921ae34110440790ea7
elvinkyungu <elvinkyungu.75@gmail.com>
2024-04-06 21:34:51 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Header from './components/HeaderContent.vue'
+import Main from './components/MainContent.vue'
+</script>
+
+<template>
+  <main class="px-32">
+    <Header />
+    <Main />
+  </main>
+</template>
+
+<style scoped>
+
+</style>
